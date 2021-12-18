8d9333c71441e8b45a8b2fcdfb79082786f2b367
Chodabeen <22000711@handong.edu>
2021-12-18 17:33:30 +0900

    1st

--- src/main/java/com/spring/board/BoardController.java
+++ src/main/java/com/spring/board/BoardController.java
@@ -9,6 +9,61 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BoardController {
+	
+	@Autowired
+	BoardService boardService;
+
+	@RequestMapping(value = "/board/list", method = RequestMethod.GET)
+	public String boardlist(Model model) {
+		model.addAttribute("list", boardService.getBoardList(null));
+		return "posts";
+	}
+	
+	
+	@RequestMapping(value = "/board/add", method = RequestMethod.GET)
+	public String addPost() {
+		return "addpostform";
+	}
+	
+	
+	@RequestMapping(value = "/board/addok", method = RequestMethod.POST)
+	public String addPostOK(BoardVO vo) {
+		if(boardService.insertBoard(vo) == 0)
+			System.out.println("데이터 추가 실패 ");
+		else
+			System.out.println("데이터 추가 성공!!!");
+		return "redirect:list";
+	}
+	
+	
+	@RequestMapping(value = "board/editpost/{id}", method = RequestMethod.GET)
+	public String editPost(@PathVariable("id") int id, Model model) {
+		BoardVO boardVO = boardService.getBoard(id);
+		model.addAttribute("boardVO", boardVO);
+		return "editform";
+	}
+	
+	
+	@RequestMapping(value = "board/editok", method = RequestMethod.POST)
+	public String editPostOK(BoardVO vo) {
+		if (boardService.updateBoard(vo) == 0)
+			System.out.println("데이터 수정 실패 ");
+		else
+			System.out.println("데이터 수정 성공!!!");
+		return "redirect:list";
+	}
+	
+	@RequestMapping(value = "/deleteok/{id}", method = RequestMethod.GET)
+	public String deletePost(@PathVariable("id") int id, Model model) {
+		if (boardService.deleteBoard(id) == 0)
+			System.out.println("데이터 삭제 실패 ");
+		else 
+			System.out.println("데이터 삭제 성공!!! ");
+		return "redirect:../list";
+	}
+	
+	
+	/*
 	@Autowired
 	BoardDAO boardDAO;
 	
@@ -58,5 +113,5 @@ public class BoardController {
 		else 
 			System.out.println("데이터 삭제 성공!!! ");
 		return "redirect:../list";
-	}
+	}*/
 }
